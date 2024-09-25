9195fba4402833e44bb7e8fc135fed13c41989f8
divy692 <divya203033@gmail.com>
2024-09-25 11:05:55 +0530

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,7 @@
+<?php
+date_default_timezone_set('Asia/Kolkata'); // Set your timezone
+$name = "Divya"; // Replace with your name
+$current_time = date('Y-m-d H:i:s'); // Get the current time
+echo "Hello $name, right now the time is $current_time";
+?>
+
